b801729105322571f2cba21396a36054c21f3039
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-09 19:47:28 -0500

    Taller 38

---
+++ src/main/java/com/example/demo/PwAutorizacionU4CaCmApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwAutorizacionU4CaCmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwAutorizacionU4CaCmApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/demo/PwAutorizacionU4CaCmApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PwAutorizacionU4CaCmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
