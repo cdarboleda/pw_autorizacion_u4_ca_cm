af57e495ac1c4c6aa7ff3280d83efe7c0a0403f0
LABORATORIO DE COMPUTADORAS FACULTAD <fing.labcom@uce.edu.ec>
2023-08-16 19:48:08 -0500

    Taller 40

--- src/main/java/com/example/demo/controller/TokenControllerRestFul.java
+++ src/main/java/com/example/demo/controller/TokenControllerRestFul.java
@@ -15,7 +15,7 @@ public class TokenControllerRestFul {
 	
 	
 	
-	@GetMapping
+	@GetMapping("/obtener")
 	public String obtenerToken(@RequestBody UsuarioTO usuario) {
 		return "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.SflKxwRJSMeKKF2QT4fwpMeJf36POk6yJV_adQssw5c";
 	}
